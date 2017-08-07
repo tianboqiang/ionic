cfb39726698952e967be29253815528c02becf00
Brandy Carney <brandy@ionic.io>
2017-08-07 13:48:09 -0400

    refactor(progress): get progress working with colors and modes
    references #6669

--- src/components/progress/progress.ts
+++ src/components/progress/progress.ts
@@ -1,32 +1,48 @@
-import { Component, Input, ViewEncapsulation, ElementRef, Renderer } from '@angular/core';
+import { Component, ElementRef, Input, Renderer, ViewEncapsulation } from '@angular/core';
 import { Ion } from '../ion';
 import { Config } from '../../config/config';
+
 export type progressIndicator = 'determinate' | 'indeterminate' | 'buffer' | 'query';
 
 @Component({
   selector: 'ion-progress',
   template : `
-    <div class="progress" [style.width.%]="value"></div>
-    <div class="animatable"></div>
-    <div class="buffer-circles"></div>
-    <div class="buffer" [style.width.%]="buffer"></div>
+    <div class="progress-bar" [style.width.%]="value"></div>
+    <div class="progress-animatable"></div>
+    <div class="progress-buffer-circles"></div>
+    <div class="progress-buffer" [style.width.%]="buffer"></div>
   `,
-  encapsulation: ViewEncapsulation.None,
-  host: {
-    '[class]': 'indicator'
-  }
+  encapsulation: ViewEncapsulation.None
 })
 export class Progress extends Ion {
 
-  constructor(config: Config, elementRef: ElementRef, renderer: Renderer) {
+  constructor(config: Config, public elementRef: ElementRef, public renderer: Renderer) {
     super(config, elementRef, renderer, 'progress');
+
+    this.indicator = this._indicator;
    }
 
   /**
    * @input {'determinate' | 'indeterminate' | 'buffer' | 'query'} The current mode of the progress indicator
    */
+  _indicator: progressIndicator = 'determinate';
+
   @Input()
-  indicator: progressIndicator = 'determinate';
+  set indicator(val: progressIndicator) {
+    if (this._indicator) {
+      this.setElementClass(`progress-${this._indicator}`, false);
+    }
+    if (val) {
+      this.setElementClass(`progress-${val}`, true);
+      this._indicator = val;
+    }
+  }
+
+  get indicator(): progressIndicator {
+    return this._indicator;
+  }
+
+
   /**
    * @input {number} The value of the progress indicator, as a percentage (out of 100)
    */
